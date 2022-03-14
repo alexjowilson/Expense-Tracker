fdfbc87aceccc70f5163aea1efa29557c5b957fb
ilh <ilh@wwu.edu>
2022-03-13 22:01:02 -0700

    Edited Filter by Year label

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -19,7 +19,7 @@ const ExpensesFilter = (props) => {
     return (
     <div className='expenses-filter'>
         <div className='expenses-filter__control'>
-        <label>Filter by year</label>
+        <label>Filter by Year</label>
         <select value={props.selected} onChange={handleDropDownChange}>
             <option value='2019'>2020</option>
             <option value='2021'>2021</option>

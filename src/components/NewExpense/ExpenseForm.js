a3e827826148b5bc736dbf7437f164bbe3175b2a
ilh <ilh@wwu.edu>
2022-03-13 19:06:21 -0700

    Edited Expense label in ExpenseForm.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -64,7 +64,7 @@ const ExpenseForm = (props) =>
     return <form onSubmit={submitHandler}>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
-                <label>Title</label>
+                <label>Expense Title</label>
                 <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
             </div>
             <div className='new-expense__control'>
